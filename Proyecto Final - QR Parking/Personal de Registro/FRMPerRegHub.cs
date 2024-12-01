4df35df5772664b75812728f108203f0d0a14257
Franya Gutierrez <168799451+franygutz@users.noreply.github.com>
2024-11-30 23:47:06 -0600

    Add files via upload

---
+++ Proyecto Final - QR Parking/Personal de Registro/FRMPerRegHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Final___QR_Parking.Personal_de_Registro
+{
+    public partial class FRMPerRegHub : Form
+    {
+        public FRMPerRegHub()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
